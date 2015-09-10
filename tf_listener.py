aa87bf5487f2d589b6be0b9e2db95a03aa060bc7
vishal-kit <vishal.kit@outlook.com>
2015-09-10 16:43:51 +0900

    Initial program

--- tf_listener.py
+++ tf_listener.py
@@ -1,15 +1,20 @@
+#This program is used to output the Rosbag file tf data for particular 
+#joints to a csv file
+#Author: Vishal Gaurav
+#Date: 10/08/2015
+
 #!/usr/bin/env python  
 import roslib
 import rospy
 import math
 import tf
-import rosbag
-import geometry_msgs.msg
+#import rosbag
+#import geometry_msgs.msg
 
 i=1
 if __name__ == '__main__':
     rospy.init_node('tf_link')
-    bag = rosbag.Bag('subset.bag')
+   # bag = rosbag.Bag('subset.bag')
     print "hi this is 1st value of i=%d" % (i)
     i=i+1
     listener = tf.TransformListener()
@@ -23,16 +28,10 @@ if __name__ == '__main__':
 	   print "8 value of i=%d" % (i)
     	   continue
         print "9 value of i=%d" % (i)
-        #i=i+1
-        #angular = 4 * math.atan2(trans[1], trans[0])
-        #linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-        #print "9 value of i=%d" % (i)
-        #i=i+1
-	    #cmd = tf.msg.Transform()
-           # cmd.linear.x = linear
-           # cmd.angular.z = angular
+        
 	print "trans= %f" %(trans[2])
         #print "10 value of i=%d" % (i)
+	
         
 	rate.sleep()
 	   # openni_vel.publish(cmd)
