5843845f1868004c1a093d8aba6a7aaa5d5a20f4
vishal-kit <vishal.kit@outlook.com>
2015-09-14 12:20:26 +0900

    csv file being saved correctly but time stamp is not

--- tf_listener.py
+++ tf_listener.py
@@ -25,12 +25,14 @@ if __name__ == '__main__':
 	header=['neck_2.x', 'neck_2.y', 'neck_2.z', 'right_shoulder_2.x', 'right_shoulder_2.y', 'right_shoulder_2.z', 'right_elbow_2.x',\
 		'right_elbow_2.y', 'right_elbow_2.z', 'right_hand_2.x', 'right_hand_2.y', 'right_hand_2.z', 'right_hip_2.x',\
 		'right_hip_2.y', 'right_hip_2.z', 'time']
-	for j in range(len(header)):	
-		if (j==len(header)):
-			f.write(header[j]+'\n')
-		else:
-			f.write(header[j]+',')
-	
+	#for j in range(len(header)):	
+	#	if (j==len(header)):
+	#		f.write(header[j]+'\n')
+	#	else:
+	#		f.write(header[j]+',')
+	f.write('neck_2.x, neck_2.y, neck_2.z, right_shoulder_2.x, right_shoulder_2.y, right_shoulder_2.z, right_elbow_2.x,')
+	f.write('right_elbow_2.y, right_elbow_2.z, right_hand_2.x, right_hand_2.y, right_hand_2.z, right_hip_2.x,')
+	f.write('right_hip_2.y, right_hip_2.z, time.sec, time.nsec\n')
 
 	listener = tf.TransformListener()
 	rate = rospy.Rate(10.0)
@@ -39,7 +41,7 @@ if __name__ == '__main__':
     			(trans,rot) = listener.lookupTransform('openni_depth_frame', 'neck_2',\
 				      rospy.Time())
     			print "2 value of i=%d" % (i)
-			i=i+1
+			
 			t=rospy.Time()
     		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			trans = [0, 0, 0]
@@ -47,16 +49,16 @@ if __name__ == '__main__':
 		try:
     			(trans1,rot1) = listener.lookupTransform('openni_depth_frame', 'right_shoulder_2',\
 				      rospy.Time())
-    			print "2 value of i=%d" % (i)
-    			i=i+1
+    			print "3 value of i=%d" % (i)
+    			
     		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			trans1 = [0, 0, 0]
     			continue
 		try:
     			(trans2,rot2) = listener.lookupTransform('openni_depth_frame', 'right_elbow_2',\
 				      rospy.Time())
-    			print "2 value of i=%d" % (i)
-    			i=i+1
+    			print "4 value of i=%d" % (i)
+    			
     		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			trans2 = [0, 0, 0]
     			continue
@@ -64,26 +66,24 @@ if __name__ == '__main__':
 		try:
     			(trans3,rot3) = listener.lookupTransform('openni_depth_frame', 'right_hand_2',\
 				      rospy.Time())
-    			print "2 value of i=%d" % (i)
-    			i=i+1
+    			print "5 value of i=%d" % (i)
+    			
     		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			trans3 = [0, 0, 0]
     			continue
 		try:
     			(trans4,rot4) = listener.lookupTransform('openni_depth_frame', 'right_hip_2',\
 				      rospy.Time())
-    			print "2 value of i=%d" % (i)
+    			print "6 value of i=%d" % (i)
     			i=i+1
     		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			trans4 = [0, 0, 0]
     			continue
-        	#print "4 value of i=%d" % (i)
-        	f.write("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d\n"%(trans[0],trans[1],trans[2],trans1[0],trans1[1],trans1[2],trans2[0],trans2[1],\
-			   trans2[2],trans3[0],trans3[1],trans3[2],trans4[0],trans4[1],trans4[2],t.secs))
-		#print "trans= %f" %(trans[2])
-        #print "10 value of i=%d" % (i)
+        
+        	f.write("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d\n"%(trans[0],trans[1],trans[2],trans1[0],\
+			trans1[1],trans1[2],trans2[0],trans2[1],trans2[2],trans3[0],trans3[1],trans3[2],trans4[0],\
+			trans4[1],trans4[2],t.secs,t.nsecs))
+		
 		rate.sleep()
-	   # openni_vel.publish(cmd)
-  
-            #rate.sleep()
+	              
 	f.close()
